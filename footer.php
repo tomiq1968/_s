2a26a9773f8269da0e07aa7ab66252b1f43bce23
gatespace <gatespace.k@gmail.com>
2013-04-22 18:24:50 +0900

    スタイルシートをsass（.scss）に変更

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 			<?php do_action( '_s_credits' ); ?>
 			<a href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', '_s' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', '_s' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', '_s' ), '_s(gatespace_theme Branch)', '<a href="http://gatespace.jp/" rel="designer">gatespace</a> (original: <a href="http://automattic.com/" rel="designer">Automattic</a>)' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
